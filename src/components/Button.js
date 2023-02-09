55914b020fa0b4e85f5a0fa54c98da1058e76028
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-02-08 21:03:52 -0600

    Added handleClick to play audio from buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const Button = () => {
+const Button = (props) => {
+    const handleClick = () => {
+        document.getElementById("Q").play();
+    };
+
     return (
         <>
-            <div className="drum-pad">Q</div>
+            <audio src="/assets/Heater-1.mp3" className="clip" id="Q"></audio>
+            <button type="button" className="drum-pad" onClick={handleClick}>{props.letter}</button>
         </>
     );
 };
