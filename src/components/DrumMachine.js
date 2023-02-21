7e51543229da196d029df345d36bd0070db7942a
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-02-21 13:14:23 -0600

    Moved handleClick from Button, added handleKeyDown functionality

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Button from "./Button";
 
 const DrumMachine = () => {
+    const handleKeyDown = (event) => {
+        document.getElementById(event.key.toUpperCase()).play();
+        console.log('A key was pressed', event.key);
+    };
+
+    const handleClick = (letter) => {
+        document.getElementById(letter).play();
+    };
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
     return (
         <>
             <div id="drum-machine">
                 <div id="drum-keys">
-                    <Button letter="Q" fileName="Heater-1" />
-                    <Button letter="W" fileName="Heater-2" />
-                    <Button letter="E" fileName="Heater-3" />
-                    <Button letter="A" fileName="Heater-4" />
-                    <Button letter="S" fileName="Clap" />
-                    <Button letter="D" fileName="Open-HH" />
-                    <Button letter="Z" fileName="Kick-n-Hat" />
-                    <Button letter="X" fileName="Kick" />
-                    <Button letter="C" fileName="Closed-HH" />
+                    <Button letter="Q" fileName="Heater-1" handleClick={handleClick} />
+                    <Button letter="W" fileName="Heater-2" handleClick={handleClick} />
+                    <Button letter="E" fileName="Heater-3" handleClick={handleClick} />
+                    <Button letter="A" fileName="Heater-4" handleClick={handleClick} />
+                    <Button letter="S" fileName="Clap" handleClick={handleClick} />
+                    <Button letter="D" fileName="Open-HH" handleClick={handleClick} />
+                    <Button letter="Z" fileName="Kick-n-Hat" handleClick={handleClick} />
+                    <Button letter="X" fileName="Kick" handleClick={handleClick} />
+                    <Button letter="C" fileName="Closed-HH" handleClick={handleClick} />
                 </div>
                 <div id="display">display</div>
             </div>
